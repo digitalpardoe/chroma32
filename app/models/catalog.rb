b79fccc15433049a2fe1dd06a903710d330dc46e
Alex Pardoe <digital.pardoe@gmail.com>
2010-02-07 18:02:03 +0000

    Can delete catalogs, the delete cascades through catalogs and documents.

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -2,8 +2,8 @@ class Catalog < ActiveRecord::Base
   validates_presence_of :name
   
   belongs_to :catalogs
-  has_many :catalogs
-  has_many :documents
+  has_many :catalogs, :dependent => :destroy
+  has_many :documents, :dependent => :destroy
   
   def self.root
     self.where(:name => "root").order("created_at ASC").limit(1).first
