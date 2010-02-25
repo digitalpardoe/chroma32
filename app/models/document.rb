5846412293eab54b1f3c0b9a9949e9b1e6b9f07d
Alex Pardoe <digital.pardoe@gmail.com>
2010-02-25 15:22:38 +0000

    Clean up 'Document' model.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,6 @@
 class Document < ActiveRecord::Base
-  validates_presence_of :name, :size, :content_type, :signature, :catalog_id
+  validates_presence_of :document
+  validates_presence_of :name, :size, :content_type, :signature, :catalog_id, :if => Proc.new { |document| document.document != nil }
   validates_uniqueness_of :name, :scope => [:catalog_id]
   
   belongs_to :catalog
@@ -11,6 +12,11 @@ class Document < ActiveRecord::Base
   
   DOCUMENT_CACHE = File.join(Rails.root, "tmp", "documents")
   
+  def file
+    File.join(DOCUMENT_CACHE, "#{self.signature}")
+  end
+  
+  private
   def persist_document
     return if !document
     
@@ -22,14 +28,21 @@ class Document < ActiveRecord::Base
     self.extension = file_ext
     self.content_type = document.content_type
     
+    save_to_disk
+    check_duplicate_names
+    register_mime_type
+  end
+  
+  def save_to_disk
     File.open(File.join(DOCUMENT_CACHE, "#{self.name}.#{self.extension}"), "wb") { |f| f.write(document.read) }
     self.signature = MD5.new(IO.read(File.join(DOCUMENT_CACHE, "#{self.name}.#{self.extension}"))).hexdigest
     
     self.size = File.size(File.join(DOCUMENT_CACHE, "#{self.name}.#{self.extension}"))
     
     File.move(File.join(DOCUMENT_CACHE, "#{self.name}.#{self.extension}"), File.join(DOCUMENT_CACHE, "#{self.signature}"))
+  end
   
-    # This bit is responsible for handling duplicate file names.
+  def check_duplicate_names
     count = 1
     name = self.name
     while (true)
@@ -40,9 +53,10 @@ class Document < ActiveRecord::Base
         break
       end
     end
-    
-    # Need to register the MIME type.
-    if !Mime::Type.lookup_by_extension(file_ext)
+  end
+  
+  def register_mime_type
+    if !Mime::Type.lookup_by_extension(self.extension)
       Mime::Type.register self.content_type, self.extension
     end
   end
@@ -51,9 +65,5 @@ class Document < ActiveRecord::Base
     if Document.where(:signature => self.signature).count(:id) <= 1
       File.delete(File.join(DOCUMENT_CACHE, "#{self.signature}"))
     end
-  end
-  
-  def file
-    File.join(DOCUMENT_CACHE, "#{self.signature}")
   end
 end
