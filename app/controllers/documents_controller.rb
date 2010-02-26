8b164709ec4187cbb0e6b9a0f6308eb80b43a217
Alex Pardoe <digital.pardoe@gmail.com>
2010-02-26 15:43:53 +0000

    Removed unnecessary complexity in file downloading.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -57,9 +57,9 @@ class DocumentsController < ApplicationController
   def download
     respond_to do |format|
        format.all {
-         @document = Document.where(:name => params[:id], :extension => request.format.symbol.to_s, :catalog_id => params[:catalog_id]).limit(1).first
+         @document = Document.where(:name => params[:id], :extension => params[:format], :catalog_id => params[:catalog_id]).limit(1).first
          # TODO: X-Sendfile support, perhaps.
-         send_file @document.file, :type => @document.content_type, :disposition => "attachment", :filename => "#{params[:id]}.#{request.format.symbol.to_s}", :stream => false
+         send_file @document.file, :type => @document.content_type, :disposition => "attachment", :filename => "#{params[:id]}.#{params[:format]}", :stream => false
        }
     end
   end
