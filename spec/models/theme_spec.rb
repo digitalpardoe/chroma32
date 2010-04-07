220fadae16e203643c751dba185e201a07ee3ef9
Alex Pardoe <digital.pardoe@gmail.com>
2010-04-07 14:35:20 +0100

    Theme testing complete.

--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Theme do
-
+  it "should list available themes" do
+    Theme.all.size > 0
+  end
+  
+  it "should return a theme of the correct name" do
+    Theme.where(:name => "chroma32").first.canonical.should == "chroma32"
+  end
+  
+  it "should change the active theme" do
+    old_theme = Setting.application.value("theme")
+    Theme.where(:name => "new_theme").first.save
+    Setting.application.value("theme").should_not == old_theme
+    Setting.application.value("theme").should == "new_theme"
+  end
 end
