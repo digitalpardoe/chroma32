a7d1a12a8259d15509340c9307ab31fb3cb039a0
Alex Pardoe <digital.pardoe@gmail.com>
2010-04-09 10:23:03 +0100

    More compatible way of loading the model extensions.

--- lib/extensions/plugins.rb
+++ lib/extensions/plugins.rb
@@ -2,7 +2,7 @@ module Plugins
   module ActiveRecord
     def acts_as_pluggable
       PLUGIN_CONFIG.each_key do |plugin|
-        model_extension = File.join(PLUGINS_DIR, plugin.to_s, 'app', 'models', 'extensions', "#{base_class.to_s.downcase}.rb")
+        model_extension = File.join(PLUGINS_DIR, plugin.to_s, 'app', 'models', 'extensions', "#{base_class.to_s.underscore}.rb")
         eval (File.open(model_extension, "r").read) if File.exists?(model_extension)
       end
     end
